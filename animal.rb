527e2672e0059fb29d97c3e05dfbdfef92aacf2e
EC2 Default User <ec2-user@ip-172-31-81-106.ec2.internal>
2020-05-17 05:57:09 +0000

    インスタンス引数を定義

--- animal.rb
+++ animal.rb
@@ -1,33 +1,32 @@
 class Animal
-    attr_accessor :name, :age, :hobby #nameとageで変数を定義
+    attr_accessor :name, :age #nameとageで変数を定義
     
     #インスタンスを初期化するメソッド
-    def initialize (name, age, hobby)#引数を渡す変数
+    def initialize (name, age)#引数を渡す変数
         self.name = name
         self.age = age
-        self.hobby = hobby
     end
     
     def say #出力を記述
-        puts "#{self.name}です。#{self.age}歳です。私は#{self.hobby}について考えます。"
+        puts "#{self.name}です。#{self.age}歳です。"
         
-        if self.age >= 25 && self.age < 30
-            puts "アラサーになりました。"
+        # if self.age >= 25 && self.age < 30
+        #     puts "アラサーになりました。"
         
-        elsif self.age >= 30
-            puts "いやーみなさんお若いですね。私は30超えていますから。"
+        # elsif self.age >= 30
+        #     puts "いやーみなさんお若いですね。私は30超えていますから。"
       
-        else self.age >= 20 && self.age < 25
-            puts "一番年下かもしれません。宜しくお願いします。"
-        end
+        # else self.age >= 20 && self.age < 25
+        #     puts "一番年下かもしれません。宜しくお願いします。"
+        # end
     end
 end    
 
-animal = Animal.new('田中　太郎', 25, '電車') #変数animalにclass Animal.new インスタンスを生成し引数を渡す
-animal.say #変数animalをsayで実行※出力が記述されているの出力される。
+# animal = Animal.new('田中　太郎', 25) #変数animalにclass Animal.new インスタンスを生成し引数を渡す
+# animal.say #変数animalをsayで実行※出力が記述されているの出力される。
 
-cat = Animal.new('鈴木　次郎', 30, '野球')
-cat.say
+# cat = Animal.new('鈴木　次郎', 30)
+# cat.say
 
-dog = Animal.new('佐藤　花子', 20, '映画')
-dog.say
+# dog = Animal.new('佐藤　花子', 20)
+# dog.say
