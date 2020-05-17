01ea5a44e4c81daa878f7154eebe7c0ccc3c9857
EC2 Default User <ec2-user@ip-172-31-81-106.ec2.internal>
2020-05-17 05:58:02 +0000

    出力と引数を定義

---
+++ main.rb
@@ -0,0 +1,19 @@
+require './human'
+require './animal'
+
+class Main
+    def self.start
+    tanaka = Human.new("田中　太郎", "25","電車")
+    suzuki = Human.new("鈴木　次郎","30","野球")
+    sato   = Human.new("佐藤 花子","20","映画")
+    
+    tanaka.say
+    tanaka.think
+    suzuki.say
+    suzuki.think
+    sato.say
+    sato.think
+    end
+end
+
+Main.start
